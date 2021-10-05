8f4830b87fe9c3349856899e1106e4c8c7c117c0
mishra06 <mishra.vibhakar7262@gmail.com>
2021-10-05 12:57:16 +0530

    "food tabs"

--- client/src/Layout/Home.layout.jsx
+++ client/src/Layout/Home.layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // Components
 import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
 
 const HomeLayout = (props) => {
   return (
@@ -9,7 +10,8 @@ const HomeLayout = (props) => {
     <Navbar />
     <div className="container mx-auto px-4 lg:px-20 ">     
       {props.children}
-      </div>  
+      </div>
+      <FoodTab/>
   </>
   );
 };
