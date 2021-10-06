9f0c866dd2d4a0b259c1533228dc3402b7a3a2a6
mishra06 <mishra.vibhakar7262@gmail.com>
2021-10-06 12:11:49 +0530

    "add delivery section"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './App.jsx';
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
