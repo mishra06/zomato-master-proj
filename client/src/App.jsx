895fbd902b753e3411eabd400c35ed2d361e5b4d
mishra06 <mishra.vibhakar7262@gmail.com>
2021-10-12 17:17:41 +0530

    " add review reducers"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,50 @@
-import { Route, Redirect, } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
 
 // HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+import CheckoutLayoutHOC from "./HOC/Checkout.Hoc";
 
-// Component
-import Temp from "./Components/temp";
-
-//pages
+// pages
 import Home from "./Page/Home";
+import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
+import Reviews from "./Page/Restaurant/Reviews";
+import Menu from "./Page/Restaurant/Menu";
+import Photos from "./Page/Restaurant/Photos";
+import Checkout from "./Page/Checkout";
+import RedirectRestaurant from "./Page/Restaurant/Redirect";
+import GoogleAuth from "./Page/GoogleAuth";
+
+// redux action
+import { getMyself } from "./Redux/Reducer/User/user.action";
+
+// axios global settings
+if (localStorage.zomatoUser) {
+  const { token } = JSON.parse(localStorage.zomatoUser);
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 function App() {
-  return  (
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.zomatoUser) dispatch(getMyself());
+  }, []);
+
+  return (
     <>
-    <Route path="/" exact>
+      <Route path="/" exact>
         <Redirect to="/delivery" />
       </Route>
-     <HomeLayoutHOC path="/:type" exact component={Home} />
-     <RestaurantLayoutHOC
+      <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
+
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+
+      <HomeLayoutHOC path="/google/:token" exact component={GoogleAuth} />
+      <RestaurantLayoutHOC
         path="/restaurant/:id/overview"
         exact
         component={Overview}
@@ -40,7 +67,16 @@ function App() {
       />
       <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
     </>
-   );
-  }
+  );
+}
 
 export default App;
+
+// :type
+
+// delivery
+// dining
+// nightlife
+// nutrition
+
+// master -> type
